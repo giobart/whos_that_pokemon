110b64b51663f96e04fa973757483dba687fb43e
Giovanni <giovanni.bartolomeo.rg@gmail.com>
2020-12-07 19:14:53 +0100

    added dataset download script

--- src/tools/dataset_tools.py
+++ src/tools/dataset_tools.py
@@ -1,12 +1,39 @@
 from os import path
 import os
 import requests
-import zipfile
+import tarfile
 from config import *
 import glob
+import gdown
 
-TARGET_FOLDER = path.join(".",DATASET_FOLDER_IMG) 
+TARGET_FOLDER = path.join(".", DATASET_FOLDER_IMG)
 FOLDER_LIST = []
+
+
+def dataset_download_targz(url=DATASET_URL):
+    folder_list = glob.glob(path.join(DATASET_FOLDER_IMG, "*"))
+    if len(folder_list) != 0:
+        print("Dataset already downloaded")
+        return
+    r = requests.get(DATASET_URL, allow_redirects=True)
+    target_filepath = path.join(".", DATASET_ZIP_NAME)
+    print(target_filepath)
+    open(target_filepath, 'wb').write(r.content)
+    with tarfile.open(target_filepath) as tar:
+        tar.extractall(path=DATASET_MAIN_FOLDER_NAME)
+
+
+def dataset_gdrive_download(url=DRIVE_URL):
+    folder_list = glob.glob(path.join(DATASET_FOLDER_IMG, "*"))
+    if len(folder_list) != 0:
+        print("Dataset already downloaded")
+        return
+    target_filepath = path.join(".", DATASET_ZIP_NAME)
+    gdown.download(url, target_filepath, quiet=False)
+    zip_file = zipfile.ZipFile(target_filepath, "r")
+    zip_file.extractall(path=DATASET_MAIN_FOLDER_NAME)
+    zip_file.close()
+
 
 def get_dataset_filename_map(min_val=2):
     """
@@ -18,16 +45,18 @@ def get_dataset_filename_map(min_val=2):
         Arguments: 
         min_val= minim amount of element that the Value list must contain. Default: 2
     """
+    FOLDER_LIST = glob.glob(path.join(DATASET_FOLDER_IMG, "*"))
+    if len(FOLDER_LIST) == 0:
+        raise (
+            "place the dataset folder into the main project folder and update the config file correspondingly or download it first with donwload_dataset()")
     result = {}
     for folder_path in FOLDER_LIST:
-        person_name=os.path.basename(os.path.normpath(folder_path))
-        image_list=glob.glob(path.join(folder_path, "*"+DATASET_IMAGE_EXTENSION))
-        if len(image_list)>=min_val:
-            result[person_name]=image_list
-            
-    return result
-        
+        person_name = os.path.basename(os.path.normpath(folder_path))
+        image_list = glob.glob(path.join(folder_path, "*" + DATASET_IMAGE_EXTENSION))
+        if len(image_list) >= min_val:
+            result[person_name] = image_list
 
-FOLDER_LIST=glob.glob( path.join(DATASET_FOLDER_IMG,"*"))
-if len(FOLDER_LIST) == 0:
-    raise("place the dataset folder into the main project folder and update the config file correspondingly")
+    return result
+
+
+
