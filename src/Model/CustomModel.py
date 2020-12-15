71700f555f8bbf528d8e86c9946c7ef14337eb60
Kareem <kareem.janou@gmail.com>
2020-12-15 23:59:28 +0100

    resloving un-commited changes

--- src/Model/CustomModel.py
+++ src/Model/CustomModel.py
@@ -3,94 +3,66 @@ from torch import nn
 import pytorch_lightning as pl
 import torch.nn.functional as F
 from src.tools.model_tools import ContrastiveLoss
+from src.Model.GeneralLayers import FCN_layer
+from src.Model.CNN_Nets import myCNN, InceptionRNV1
+from enum import Enum
+from pytorch_lightning.metrics.functional import accuracy
+from torch.optim.lr_scheduler import StepLR
 
 
-class ConvPoolPRelu(nn.Module):
-    def __init__(self, in_channels, out_channels, kernel_size, padding, activ_fn=nn.PReLU):
-        super().__init__()
+class CNN_MODEL(Enum):
+    MyCNN = 1
+    InceptionResnetV1 = 2
 
-        self.model = nn.Sequential(
-            nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, padding=padding),
-            nn.MaxPool2d(2, 2),
-            activ_fn(),
-            nn.BatchNorm2d(out_channels),
-        )
-
-        def weights_init(m):
-            if type(m) == nn.Conv2d:
-                nn.init.xavier_normal_(m.weight, gain=2.0)
-
-        self.model.apply(weights_init)
-
-    def forward(self, x):
-        return self.model(x)
-
-
-class CNN(nn.Module):
-    def __init__(self, activ_fn, f_size, f_channels, padding=0, input_channels=3):
-        super().__init__()
-
-        self.model = nn.Sequential(
-            ConvPoolPRelu(input_channels, f_channels, f_size, padding, activ_fn=activ_fn),  # 128x128xf
-            ConvPoolPRelu(f_channels, f_channels * 2, f_size, padding, activ_fn=activ_fn), #64x64x2f
-            ConvPoolPRelu(f_channels * 2, f_channels * 4, f_size, padding, activ_fn=activ_fn), #32x32x4f
-            # ConvPoolPRelu(f_channels * 4, f_channels * 8, f_size, padding, activ_fn=activ_fn), #16x16x8f
-        )
-
-    def forward(self, x):
-        return self.model(x)
-
-class FCN_layer(nn.Module):
-    def __init__(self, input_size, output_size, dropout=0):
-        super().__init__()
-
-        self.model = nn.Sequential(
-            nn.Linear(input_size, output_size),
-            nn.PReLU(),
-            nn.BatchNorm1d(output_size),
-            nn.Dropout(p=dropout),
-        )
-
-        def weights_init(m):
-            if type(m) == nn.Linear:
-                nn.init.xavier_normal_(m.weight, gain=2.0)
-
-        self.model.apply(weights_init)
-
-    def forward(self, x):
-        return self.model(x)
 
 class Siamese(pl.LightningModule):
-    def __init__(self, hparams=None, input_shape=(3, 128, 128), CNN_model=CNN):
+    def __init__(self, hparams=None, scheduler_params=None, cnn_model=CNN_MODEL.MyCNN, freeze_layers=True):
         super().__init__()
         self.hparams = hparams
-        input_channels, height, width = input_shape
+        self.loss_fn = hparams['loss_fn']
+        self.scheduler_params = scheduler_params
+
         # CNN
-        self.conv = CNN_model(nn.PReLU,
+        if cnn_model == CNN_MODEL.MyCNN:
+            self.conv = myCNN(nn.PReLU,
                               hparams["filter_size"],
                               hparams["filter_channels"],
                               padding=int(hparams["filter_size"] / 2),
-                              input_channels=input_channels)  # size/8 x 4*channels
+                              )  # size/8 x 4*channels
+        elif cnn_model == CNN_MODEL.InceptionResnetV1:
+            self.conv = InceptionRNV1(freeze_layers=freeze_layers)
+        else:
+            raise Exception("cnn_model is not defined correctly")
 
-        self.loss_fn = hparams['loss_fn']
+        self.input_size = self.conv.input_size
+        self.cnn_output_size = self.conv.output_size
+
         if isinstance(self.loss_fn, ContrastiveLoss):
+            self.loss_fn.margin = hparams['loss_margin']
             self.linear = nn.Sequential(
-                FCN_layer(int(hparams["filter_channels"] * 4 * int(height / 8) * int(width / 8)),
-                          hparams['n_hidden1'],
-                          dropout=hparams['dropout']),
+                FCN_layer(self.cnn_output_size, hparams['n_hidden1'], dropout=hparams['dropout']),
                 FCN_layer(hparams['n_hidden1'], hparams['n_hidden2']),
                 nn.Linear(hparams['n_hidden2'], hparams['n_hidden3'])
             )
         elif isinstance(self.loss_fn, nn.BCEWithLogitsLoss):
-            self.loss_fn.margin = hparams['loss_margin']
-            self.linear = FCN_layer(int(hparams["filter_channels"] * 4 * int(height / 8) * int(width / 8)),
-                                    hparams['n_hidden1'])
+            self.linear = FCN_layer(self.cnn_output_size, hparams['n_hidden1'])
             self.out = nn.Sequential(
+                # FCN_layer(hparams['n_hidden1'], 1, dropout=hparams['dropout']),
                 FCN_layer(hparams['n_hidden1'], hparams['n_hidden2'], dropout=hparams['dropout']),
                 nn.Linear(hparams['n_hidden2'], 1)
             )
         else:
             raise Exception('loss_fn is not well defined')
+
+        self.params_to_update = []
+        print("Layers to update")
+        if freeze_layers:
+            for name, param in self.named_parameters():
+                if param.requires_grad:
+                    self.params_to_update.append(param)
+                    print("\t", name)
+        else:
+            self.params_to_update = self.parameters()
 
     def forward_one(self, x):
         x = self.conv(x)
@@ -109,30 +81,40 @@ class Siamese(pl.LightningModule):
             raise Exception('loss_fn is not well defined')
 
     def configure_optimizers(self):
-        optimizer = torch.optim.Adam(self.parameters(), lr=self.hparams["lr"], weight_decay=self.hparams["weight_decay"])
-        return optimizer
 
-    def calc_acc(self, y, output, beta=1.0, boundary=0.995):
+        optimizer = torch.optim.Adam(self.params_to_update, lr=self.hparams["lr"],
+                                     weight_decay=self.hparams["weight_decay"])
+
+        if self.scheduler_params is not None:
+            lr_scheduler = StepLR(optimizer, **self.scheduler_params)
+            return [optimizer], [lr_scheduler]
+        else:
+            return optimizer
+
+    def calc_acc(self, y, logits, beta=1.0, boundary=0.995):
         if isinstance(self.loss_fn, ContrastiveLoss):
-            pred = (output > boundary )
+            pred = (logits > boundary )
             # return torch.tensor(torch.sum(y == pred).item() / (len(y) * 1.0))
             tp = ( 1 + beta * beta ) * torch.sum( y * pred == 1 ).item()
             fn = ( beta * beta ) * torch.sum( y > pred ).item()
             fp = torch.sum( y < pred ).item()
             return torch.tensor( tp / ( tp + fn + fp ) )
         else:
-            y_hat = nn.Sigmoid()(output)
-            pred = (y_hat > 0.5)
-            return torch.tensor(torch.sum(y == pred).item() / (len(y) * 1.0))
+            y_hat = nn.Sigmoid()(logits).squeeze()
+            y_hat[y_hat >= 0.5] = 1.0
+            y_hat[y_hat < 0.5] = 0.0
+            return accuracy(y_hat, y)
+            # return torch.tensor(torch.sum(y == y_hat).item() / (len(y) * 1.0))
 
     def general_step(self, batch):
         x1, x2, y = batch
         output = self(x1, x2)
-        loss = self.loss_fn(output, y)
-        n_correct = torch.tensor(0.0)
+
         if isinstance(self.loss_fn, ContrastiveLoss):
+            loss = self.loss_fn(output, y)
             n_correct = self.calc_acc(y.detach().cpu(), output.detach().cpu() )
         if isinstance(self.loss_fn, nn.BCEWithLogitsLoss):
+            loss = self.loss_fn(output.squeeze(), y)
             n_correct = self.calc_acc(y.detach().cpu(), output.detach().cpu() )
 
         return loss, n_correct
