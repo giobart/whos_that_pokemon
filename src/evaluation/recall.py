feb3731f7ba584d84222bb5358df30e5bf01efde
yaliao <yaliao@ucsd.edu>
2021-01-15 14:43:39 +0100

    Implemented accuracy for k-nearest neighbors
    Also created a new function calc_f_measure to calculate F measure with regards to the 1-nearest neighbor being within the threshold or not based on its label

--- src/evaluation/recall.py
+++ src/evaluation/recall.py
@@ -23,11 +23,40 @@ def assign_by_euclidian_at_k(X, T, k):
 #     return indices, distances
 
 # TODO: calc acc similar to calc_recall but add restriction on the distance.
-def calc_acc_sim_at_k(distance, indices, thr=0.95):
-    pass
+def calc_acc_sim_at_k(T, k, distance, indices, thr=0.95):
+    """
+    T : [nb_samples] (target labels)
+    k : number of neighbors
+    distance : 2-d array of distances between images by index
+    thr : threshold under which the distance must be
+    """
+    res = 0
+    for image, neighbors in enumerate( indices[:, :k] ):
+        for neighbor in neighbors:
+            if distance[image, neighbor] < thr and T[image] == T[neighbor]:
+                res += 1
+                break
+    return res
+    # res = sum( [1 for image, neighbors in enumerate( indices[:, :k] ) for mario in neighbors if dist[mario] < thr and lbls[mario] == lbls[image]] )
     # s = sum([1 for img_idx, closest_idx in enumerate(indices[:, 0]) if distance[img_idx, closest_idx]<thr])
     # return s / (1. * len(indices))
 
+def calc_f_measure(T, distance, indices, beta=1.0, thr=0.95):
+    """
+    beta : beta value for calculating F-measure, can be adjusted
+    """
+    tp, fp, fn = 0, 0, 0
+    for image, neighbor in enumerate( indices[:, 0] ):
+        if distance[image, neighbor] < thr and T[image] == T[neighbor]:
+            tp += 1
+        elif distance[image, neighbor] > thr and T[image] == T[neighbor]:
+            fn += 1
+        elif distance[image, neighbor] < thr and T[image] != T[neighbor]:
+            fp += 1
+    tp *= (1 + beta + beta)
+    fn *= (beta * beta)
+    return ( tp / (tp + fp + fn) )
+    
 def calc_recall_at_k(T, Y, k):
     """
     T : [nb_samples] (target labels)
